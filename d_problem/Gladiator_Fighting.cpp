0b0313701ff8c8d8c34f7dacbd3df4da76fbef80
meherab nahin <nahinrahman87@gmail.com>
2025-11-30 14:18:10 +0600

    galdiot fighting

---
+++ d_problem/Gladiator_Fighting.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;cin>>t;
+  while(t--){
+    int n;cin>>n;
+    int mx=((n-2)*(n-1))/2;
+    int mn=n-2;
+    cout<<mn<<" "<<mx<<endl;
+  }  
+    
+
+  
+   
+  return 0;
+}
+
+//min--> always lower skill win korabo.prothom e 0+rest of fight 1.
+//max-->always ekta gladitor ke win korabo.prothome 0+rest of fight e 1 kore barte thakbe.that means natural number er summation.
+
